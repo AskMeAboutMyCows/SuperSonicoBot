81b0ea105dffc671abbb8a2d297fe0bd02b074e4
Hunter <fffarmer@protonmail.com>
2022-05-12 18:52:01 -0400

    gang gang

--- index.js
+++ index.js
@@ -10,5 +10,17 @@ const TOKEN = process.env.TOKEN
 
 const LOAD_SLASH = process.argv[2] == "load"
 const APP_ID = "974424468174798858"
-const SERVER_ID ="550806423987027989" // this is for nootnoot (grap by right clicking server name top right: must have dev mode enabled is discord)
+const GUILD_ID ="550806423987027989" // this is for nootnoot (grap by right clicking server name top right: must have dev mode enabled is discord)
 
+const client = new Discord.Client({
+    intents:[
+        "GUILDS",
+        "GUILDS_VOICE_STATES"
+    ]
+})
+
+client.slashcommands = new Discord.Collection()
+client.player = new Player(client, {
+    ytdlOptions : {
+        quality : "highestaudio",
+        highWaterMark : 1 << 25
