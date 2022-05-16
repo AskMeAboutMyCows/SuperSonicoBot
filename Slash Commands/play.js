d3f8c75ddd7397acc8e803d62a8efc355ee6f1be
Hunter <fffarmer@protonmail.com>
2022-05-16 12:12:30 -0400

    add play.js and sub commands

---
+++ Slash Commands/play.js
@@ -0,0 +1,18 @@
+const {SlashCommandBuilder} = require("@discordjs/builders")
+const {MessageEmbed} = require("discord.js")
+const { QueryType } = require("discord-player")
+
+module.exports = {
+    data = new SlashCommandBuilder()
+        .setName("play")
+        .setDescription("Plays a song")
+        .addSubcommand((subcommand)=>
+            subcommand.setName("song")
+                .setDescription("loads a single song from a url")
+                .addStringOption((option)=> option.setName("url").setDescription("the url of the song").setRequired(true))
+        )
+        .addSubcommand((subcommand)=>
+            subcommand.setName("playlist")
+                .setDescription("loads a playlist from a url")
+                .addStringOption((option)=> option.setName("url").setDescription("the url of the playlist").setRequired(true))
+}
