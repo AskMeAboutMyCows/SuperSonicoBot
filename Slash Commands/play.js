6df9fe725465b2b1c1c60b1ba3cd294e6e7aaf39
Hunter <fffarmer@protonmail.com>
2022-05-17 13:12:35 -0400

    some  functions added but idk where the  brackets the guy is adding are coming from

--- Slash Commands/play.js
+++ Slash Commands/play.js
@@ -15,4 +15,8 @@ module.exports = {
             subcommand.setName("playlist")
                 .setDescription("loads a playlist from a url")
                 .addStringOption((option)=> option.setName("url").setDescription("the url of the playlist").setRequired(true))
+        .addSubcommand.setName("search").setDescription("searches for a song based on keywords and plays it"))
+        .addStringOption ((option)=> option.setName("searchterms").setDescription("the search keyword").setRequired(true))
+    
+
 }
